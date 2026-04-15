d6ff34b3a5ff1d58869107392f83de39962f9bde
jyotiram kokane <jyotiramkokane113@gmail.com>
2026-04-15 17:51:43 +0530

    login page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:toolkit_manager/features/auth/presentation/pages/register_page.dart';
+import 'package:toolkit_manager/features/auth/presentation/pages/login_page.dart';
+// import 'package:toolkit_manager/features/home/admin_home.dart;
 
 
 Future<void> main() async {
@@ -15,9 +17,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+   return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegisterPage(), // ✅ THIS IS IMPORTANT
+
+      // 🔥 IMPORTANT
+      initialRoute: "/register",
+
+      routes: {
+        "/register": (context) => const RegisterPage(),
+        "/login": (context) => const LoginPage(), // ✅ FIX HERE
+          // "/studentHome": (context) => const StudentHome(),
+          // "/adminHome": (context) => const AdminHome(),
+          // "/labHome": (context) => const LabHome(),
+
+      },
     );
   }
 }
